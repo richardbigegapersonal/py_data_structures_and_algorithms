b7040230b9eaf01ca9636819468a5d687d721594
Richard Bigega <89564469+richardbigegapersonal@users.noreply.github.com>
2025-08-10 14:53:39 -0500

    Update LinkedList.py -- adding returns true to append and prepend

--- LinkedList.py
+++ LinkedList.py
@@ -7,7 +7,7 @@ class LinkedList:
   #Let's define the constructor 
   def __init__(self,value):
     """
-    This contructs an object of Node type with initial value passed to this contructor
+    This contructs an oblect of Node type with initial value passed to this contructor
     and both head and tail pointing to it and initial length of 1.
     -self: tells python this is a mothod inside a class and not a function
     -value: this will be used to create the first node when we initialize the class
@@ -30,7 +30,11 @@ class LinkedList:
       self.tail.next=new_node
       self.tail=new_node
     self.length+=1
+    return True
   def pop(self):
+    """
+    The pop method removes and returns the last node of the linked list
+    """
     if self.head is None:
       return None
     else:
@@ -58,5 +62,6 @@ class LinkedList:
       new_node.next=self.head
       self.head=new_node
     self.length+=1
+    return True
 
       
