e62388bf3e8ca09ff809a17f1e75addb96b6808c
Richard Bigega <89564469+richardbigegapersonal@users.noreply.github.com>
2025-08-10 14:52:01 -0500

    Update LinkedList.py -- adding prepend function

--- LinkedList.py
+++ LinkedList.py
@@ -46,5 +46,17 @@ class LinkedList:
         self.head=None
         self.tail=None
       return temp
+  def prepend(self,value):
+    """
+    The passed value the method is added at the first location in the linkedList
+    """
+    new_node=Node(value)
+    if self.head is None:
+      self.head=new_node
+      self.tail=new_node
+    else:
+      new_node.next=self.head
+      self.head=new_node
+    self.length+=1
 
       
