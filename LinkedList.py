fb5feb5c1f183b23fbe57acacb7f1f74e8890fb5
Richard Bigega <89564469+richardbigegapersonal@users.noreply.github.com>
2025-08-10 20:01:51 -0500

    Update LinkedList.py -- Added more methods

--- LinkedList.py
+++ LinkedList.py
@@ -83,6 +83,50 @@ class LinkedList:
       for _ in range(index):
         temp=temp.next
     return temp
+  def set_value(self,index,value):
+    temp=self.get(index)
+    if temp:
+      temp.value=value
+      return True
+    else:
+      return False
+  def insert(self,index,value):
+    
+    if (index<0 or index>self.length):
+      return False
+    if index==0:
+      return self.prepend(value)
+    if index==self.length-1:
+      return self.append(value)
+    new_node=Node(value)
+    temp=self.get(index-1)
+    new_node.next=temp.next
+    temp.next=new_node
+    self.length+=1
+    return True
+  def remove(self,index):
+    if (index<0 or index>self.length):
+      return None
+    if index==0:
+      self.pop_first()
+    if index==self.length-1:
+      self.pop()
+    prev=self.get(index-1)
+    current=prev.next
+    prev.next=current.next
+    current.next=None
+    self.length-=1
+    return current
+  def reverse(self):
+    temp=self.head
+    self.head=self.tail
+    self.tail=temp
+    after=temp.next
+    before=None
+    for _ in range(self.length):
+      after=temp.next
+      temp.next=before
+      before=temp
+      temp=after
 
 
-      
